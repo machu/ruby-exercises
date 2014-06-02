21bbcfd876ce059f3857d77080f2d1e3043762d3
MATSUOKA Kohei <kohei@machu.jp>
2014-06-03 02:22:24 +0900

    add a question

---
+++ a1.rb
@@ -0,0 +1,15 @@
+# Arrayオブジェクトの要素が奇数の項目を全て足すコードを書いてください。
+# 例えば numbers = [2,3,5,7,11] の場合、 (3+5+7+11=) 26 が表示されればOKです。
+# 
+# ヒント：奇数かどうか調べるのは Fixnum#odd? メソッド
+# ちなみに偶数か調べるのは Fixnum#even? メソッドです。
+
+def sum_only_odd(numbers)
+	sum = 0
+	# write your code here!
+
+	sum
+end
+
+numbers = [2, 3, 5, 7, 11]
+puts sum_only_odd(numbers)
